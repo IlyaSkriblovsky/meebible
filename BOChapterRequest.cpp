143c274b57b550fe55869dcea8ac7d7c4f6c504b
Ilya Skriblovsky <ilyaskriblovsky@gmail.com>
2011-07-20 17:42:02 +0400

    Default ok button in TranslationDialog. started to implement BibleOnline.ru parsing

--- BOChapterRequest.cpp
+++ BOChapterRequest.cpp
@@ -5,6 +5,8 @@
 #include <QBuffer>
 #include <QFile>
 
+#include <QDomDocument>
+
 #include "BORusTranslation.h"
 
 BOChapterRequest::BOChapterRequest(
@@ -20,21 +22,58 @@ BOChapterRequest::BOChapterRequest(
 
 void BOChapterRequest::onNReplyFinished()
 {
-    QByteArray content = _nreply->readAll();
+//    QByteArray content = _nreply->readAll();
+//
+//    QXmlQuery query(QXmlQuery::XSLT20);
+//    query.setFocus(QString::fromUtf8(content));
+//
+//    QFile xslt(":/bo.xslt");
+//    xslt.open(QIODevice::ReadOnly);
+//    query.setQuery(&xslt);
+//
+//    QBuffer buffer;
+//    buffer.open(QIODevice::WriteOnly);
+//
+//    query.evaluateTo(&buffer);
+//
+//    buffer.close();
+//
+//    finished(buffer.buffer());
 
-    QXmlQuery query(QXmlQuery::XSLT20);
-    query.setFocus(QString::fromUtf8(content));
+    QString content = QString::fromUtf8(_nreply->readAll());
 
-    QFile xslt(":/bo.xslt");
-    xslt.open(QIODevice::ReadOnly);
-    query.setQuery(&xslt);
+    qDebug() << content;
+    qDebug() << "=============";
 
-    QBuffer buffer;
-    buffer.open(QIODevice::WriteOnly);
+    QRegExp re("<span id=\"v(\\d+)\" class=\"v\"><sup>[^<]*</sup>([^<]*)<br /></span>");
 
-    query.evaluateTo(&buffer);
+    int pos = 0;
+    while ((pos = re.indexIn(content, pos)) != -1)
+    {
+        qDebug() << re.cap(1) << re.cap(2);
+        pos += re.matchedLength();
+    }
 
-    buffer.close();
 
-    finished(buffer.buffer());
+    QDomDocument doc;
+    QDomElement html = doc.createElement("html");
+    doc.appendChild(html);
+
+    QDomElement body = doc.createElement("body");
+    html.appendChild(body);
+
+    QDomElement h3 = doc.createElement("h3");
+    body.appendChild(h3);
+
+    QDomText chapter = doc.createTextNode(
+        QString("%1 %2:1-%3")
+            .arg(translation()->bookName(bookCode()))
+            .arg(chapterNo())
+            .arg(translation()->versesInChapter(bookCode(), chapterNo()))
+    );
+
+    h3.appendChild(chapter);
+
+
+    finished(doc.toString().toUtf8());
 }
