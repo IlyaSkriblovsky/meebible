634884932037619fd6dc1b9244f6664169f5f4eb
Ilya Skriblovsky <ilyaskriblovsky@gmail.com>
2011-10-27 11:57:45 +0300

    Conditional splash images

--- meebible-invoker.sh
+++ meebible-invoker.sh
@@ -1,1 +1,5 @@
-/usr/bin/invoker -s --type=d --splash=/opt/meebible/share/splash.png --splash-landscape=/opt/meebible/share/splash-ls.png /opt/meebible/bin/meebible
+if grep inverted=true /home/user/.config/MeeBible/MeeBible.conf ; then
+    /usr/bin/invoker -s --type=d --splash=/opt/meebible/share/splash-inverted.png --splash-landscape=/opt/meebible/share/splash-ls-inverted.png /opt/meebible/bin/meebible
+else
+    /usr/bin/invoker -s --type=d --splash=/opt/meebible/share/splash.png --splash-landscape=/opt/meebible/share/splash-ls.png /opt/meebible/bin/meebible
+fi
