b177b5f74fc5e8455ef7edd434d7327d5fce72b1
Ilya Skriblovsky <ilyaskriblovsky@gmail.com>
2011-10-27 22:26:08 +0300

    All strings localized to russian

--- translations/meebible_ru.ts
+++ translations/meebible_ru.ts
@@ -13,89 +13,89 @@
     </message>
     <message>
         <source>Currently following Bible translations are supported: &lt;ul&gt; &lt;li&gt;New World Translation in 40 languages&lt;/li&gt; &lt;li&gt;Russian Synodal translation&lt;/li&gt; &lt;/ul&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>На данный момент поддерживаются следующие переводы Библии: &lt;ul&gt; &lt;li&gt;Перевод нового мира на 40 различных языках&lt;/li&gt; &lt;li&gt;Русский Синодальный перевод&lt;/li&gt; &lt;/ul&gt;</translation>
     </message>
     <message>
         <source>Read God&apos;s Word daily!</source>
-        <translation type="unfinished"></translation>
+        <translation>Читайте Божье Слово каждый день!</translation>
     </message>
     <message>
         <source>© 2011 Ilya Skriblovsky&lt;br&gt;E-Mail: &lt;a href=&quot;mailto:IlyaSkriblovsky@gmail.com&quot;&gt;Ilya.Skriblovsky@gmail.com&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>© 2011 Илья Скрибловский&lt;br&gt;E-Mail: &lt;a href=&quot;mailto:IlyaSkriblovsky@gmail.com&quot;&gt;Ilya.Skriblovsky@gmail.com&lt;/a&gt;</translation>
     </message>
     <message>
         <source>Please contact me with any comments or suggestions or if you want to add new Bible or UI translations.</source>
-        <translation type="unfinished"></translation>
+        <translation>Присылайте мне любые пожелания или комментарии, а также пишите если вы хотите добавить новые переводы Библии и интерфейса программы.</translation>
     </message>
     <message>
         <source>Homepage &amp; sources: &lt;a href=&quot;http://projects.developer.nokia.com/meebible&quot;&gt;http://projects.developer.nokia.com/meebible&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Домашная страничка и исходные коды: &lt;a href=&quot;http://projects.developer.nokia.com/meebible&quot;&gt;http://projects.developer.nokia.com/meebible&lt;/a&gt;</translation>
     </message>
     <message>
         <source>New World Translation © Watch Tower Bible and Tract Society of Pennsylvania</source>
-        <translation type="unfinished"></translation>
+        <translation>Перевод нового мира © Watch Tower Bible and Tract Society of Pennsylvania</translation>
     </message>
 </context>
 <context>
     <name>BibleView</name>
     <message>
         <source>Current translation doesn&apos;t contain this book</source>
-        <translation type="obsolete">Данный перевод не содержит этой главы</translation>
+        <translation>Данный перевод не содержит этой главы</translation>
     </message>
     <message>
         <source>&lt;h3&gt;Cannot load chapter&lt;/h3&gt; Please check your internet connection</source>
-        <translation type="obsolete">&lt;h3&gt;Не удалось загрузить главу&lt;/h3&gt; Пожалуйста, проверьте подключение к интернету</translation>
+        <translation>&lt;h3&gt;Не удалось загрузить главу&lt;/h3&gt; Пожалуйста, проверьте подключение к интернету</translation>
     </message>
 </context>
 <context>
     <name>BibleViewPage</name>
     <message>
         <source>Please notice</source>
-        <translation type="unfinished"></translation>
+        <translation>Обратите внимание</translation>
     </message>
     <message>
         <source>Full-text search works only for downloaded chapters. If you want to search whole Bible you must download it first using the app menu.</source>
-        <translation type="unfinished"></translation>
+        <translation>Полнотекстовый поиск работает только по уже закачанным главам. Если вы хотите искать по всей Библии, вам нужно скачать её всю с помощью пункта меню программы.</translation>
     </message>
     <message>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>ОК</translation>
     </message>
     <message>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation>Готово</translation>
     </message>
     <message>
         <source>Are you ready to download whole Bible?</source>
-        <translation type="unfinished"></translation>
+        <translation>Вы готовы скачать Библию целиком?</translation>
     </message>
     <message>
         <source>This may take 10–15 minutes and you&apos;d better do it over Wi-Fi connection</source>
-        <translation type="unfinished"></translation>
+        <translation>Это займёт 10–15 минут и лучше делать это через Wi-Fi-соединение</translation>
     </message>
     <message>
         <source>Download</source>
-        <translation type="unfinished"></translation>
+        <translation>Скачать</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Отмена</translation>
     </message>
     <message>
         <source>Font size</source>
-        <translation type="unfinished"></translation>
+        <translation>Размер шрифта</translation>
     </message>
     <message>
         <source>Line spacing</source>
-        <translation type="unfinished"></translation>
+        <translation>Межстрочный интервал</translation>
     </message>
     <message>
         <source>Download Bible</source>
-        <translation type="unfinished"></translation>
+        <translation>Скачать Библию</translation>
     </message>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Настройки</translation>
     </message>
     <message>
         <source>About MeeBible</source>
@@ -106,109 +106,109 @@
     <name>FetcherDialog</name>
     <message>
         <source>Downloading Bible</source>
-        <translation type="unfinished"></translation>
+        <translation>Скачивание Библии</translation>
     </message>
     <message>
         <source>Connecting...</source>
-        <translation type="unfinished"></translation>
+        <translation>Соединение...</translation>
     </message>
     <message>
         <source>Nothing to do</source>
-        <translation type="unfinished"></translation>
+        <translation>Нечего делать</translation>
     </message>
     <message>
         <source>You already have downloaded whole Bible in selected language and translation</source>
-        <translation type="unfinished"></translation>
+        <translation>Вы уже скачали этот перевод Библии целиком</translation>
     </message>
     <message>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>ОК</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Отмена</translation>
     </message>
 </context>
 <context>
     <name>LanguageDialog</name>
     <message>
         <source>Select Language</source>
-        <translation type="unfinished"></translation>
+        <translation>Выберите язык</translation>
     </message>
 </context>
 <context>
     <name>NWTranslation</name>
     <message>
         <source>New World Translation</source>
-        <translation type="obsolete">Перевод Нового мира</translation>
+        <translation>Перевод Нового мира</translation>
     </message>
 </context>
 <context>
     <name>PlaceDialog</name>
     <message>
         <source>Select Book</source>
-        <translation type="unfinished"></translation>
+        <translation>Выберите книгу</translation>
     </message>
     <message>
         <source>Chapter:</source>
-        <translation type="unfinished"></translation>
+        <translation>Глава:</translation>
     </message>
     <message>
         <source>Verse:</source>
-        <translation type="unfinished"></translation>
+        <translation>Стих:</translation>
     </message>
     <message>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>ОК</translation>
     </message>
 </context>
 <context>
     <name>SearchDialog</name>
     <message>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Закрыть</translation>
     </message>
     <message>
         <source>Search...</source>
-        <translation type="unfinished"></translation>
+        <translation>Поиск...</translation>
     </message>
     <message>
         <source>Nothing found</source>
-        <translation type="unfinished"></translation>
+        <translation>Ничего не найдено</translation>
     </message>
 </context>
 <context>
     <name>SettingsPage</name>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Настройки</translation>
     </message>
     <message>
         <source>Language</source>
-        <translation type="unfinished"></translation>
+        <translation>Язык</translation>
     </message>
     <message>
         <source>Translation</source>
-        <translation type="unfinished"></translation>
+        <translation>Перевод</translation>
     </message>
     <message>
         <source>Scroll header with the text</source>
-        <translation type="unfinished"></translation>
+        <translation>Прокручивать заголовок вместе с текстом</translation>
     </message>
     <message>
         <source>Fullscreen</source>
-        <translation type="unfinished"></translation>
+        <translation>На весь экран</translation>
     </message>
     <message>
         <source>Inverted theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Инверсная тема</translation>
     </message>
 </context>
 <context>
     <name>TranslationDialog</name>
     <message>
         <source>Select Translation</source>
-        <translation type="unfinished"></translation>
+        <translation>Выберите перевод</translation>
     </message>
 </context>
 </TS>
