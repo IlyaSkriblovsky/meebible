808c5ed1d98bfd85048e09304d21f29c1cd62643
Ilya Skriblovsky <ilyaskriblovsky@gmail.com>
2011-10-27 21:58:18 +0300

    Translated some phrases to Russian

--- translations/meebible_ru.ts
+++ translations/meebible_ru.ts
@@ -2,13 +2,137 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="ru_RU">
 <context>
-    <name>BibleView</name>
+    <name>AboutPage</name>
     <message>
-        <source>Current translation doesn&apos;t contain this book</source>
+        <source>About MeeBible</source>
+        <translation>О программе MeeBible</translation>
+    </message>
+    <message>
+        <source>Thank you for using MeeBible, the open source Bible reader for Harmattan</source>
+        <translation>Спасибо за использование MeeBible, читалки Библии для MeeGo Harmattan</translation>
+    </message>
+    <message>
+        <source>Currently following Bible translations are supported: &lt;ul&gt; &lt;li&gt;New World Translation in 40 languages&lt;/li&gt; &lt;li&gt;Russian Synodal translation&lt;/li&gt; &lt;/ul&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <source>Read God&apos;s Word daily!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>© 2011 Ilya Skriblovsky&lt;br&gt;E-Mail: &lt;a href=&quot;mailto:IlyaSkriblovsky@gmail.com&quot;&gt;Ilya.Skriblovsky@gmail.com&lt;/a&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please contact me with any comments or suggestions or if you want to add new Bible or UI translations.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Homepage &amp; sources: &lt;a href=&quot;http://projects.developer.nokia.com/meebible&quot;&gt;http://projects.developer.nokia.com/meebible&lt;/a&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>New World Translation © Watch Tower Bible and Tract Society of Pennsylvania</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BibleView</name>
+    <message>
+        <source>Current translation doesn&apos;t contain this book</source>
+        <translation type="obsolete">Данный перевод не содержит этой главы</translation>
+    </message>
+    <message>
         <source>&lt;h3&gt;Cannot load chapter&lt;/h3&gt; Please check your internet connection</source>
+        <translation type="obsolete">&lt;h3&gt;Не удалось загрузить главу&lt;/h3&gt; Пожалуйста, проверьте подключение к интернету</translation>
+    </message>
+</context>
+<context>
+    <name>BibleViewPage</name>
+    <message>
+        <source>Please notice</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Full-text search works only for downloaded chapters. If you want to search whole Bible you must download it first using the app menu.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Done</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Are you ready to download whole Bible?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This may take 10–15 minutes and you&apos;d better do it over Wi-Fi connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Download</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Font size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Line spacing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Download Bible</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About MeeBible</source>
+        <translation>О программе MeeBible</translation>
+    </message>
+</context>
+<context>
+    <name>FetcherDialog</name>
+    <message>
+        <source>Downloading Bible</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Connecting...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Nothing to do</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You already have downloaded whole Bible in selected language and translation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LanguageDialog</name>
+    <message>
+        <source>Select Language</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -16,6 +140,74 @@
     <name>NWTranslation</name>
     <message>
         <source>New World Translation</source>
+        <translation type="obsolete">Перевод Нового мира</translation>
+    </message>
+</context>
+<context>
+    <name>PlaceDialog</name>
+    <message>
+        <source>Select Book</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Chapter:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Verse:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SearchDialog</name>
+    <message>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Search...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Nothing found</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Translation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Scroll header with the text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Fullscreen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Inverted theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TranslationDialog</name>
+    <message>
+        <source>Select Translation</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
