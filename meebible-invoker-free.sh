cc246ca8ec7b2590d8fac3866b39c5aace9a8f2a
Ilya Skriblovsky <ilyaskriblovsky@gmail.com>
2012-01-10 15:33:02 +0400

    Fixed invoker script for free version

--- meebible-invoker-free.sh
+++ meebible-invoker-free.sh
@@ -1,5 +1,5 @@
 if grep inverted=true /home/user/.config/MeeBible/MeeBible.conf ; then
-    /usr/bin/invoker -s --type=d --splash=/opt/meebible-free/share/splash-inverted.png --splash-landscape=/opt/meebible/share/splash-ls-inverted.png /opt/meebible-free/bin/meebible
+    /usr/bin/invoker -s --type=d --splash=/opt/meebible-free/share/splash-inverted.png --splash-landscape=/opt/meebible-free/share/splash-ls-inverted.png /opt/meebible-free/bin/meebible
 else
-    /usr/bin/invoker -s --type=d --splash=/opt/meebible-free/share/splash.png --splash-landscape=/opt/meebible/share/splash-ls.png /opt/meebible-free/bin/meebible
+    /usr/bin/invoker -s --type=d --splash=/opt/meebible-free/share/splash.png --splash-landscape=/opt/meebible-free/share/splash-ls.png /opt/meebible-free/bin/meebible
 fi
