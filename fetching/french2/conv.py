71651a7d92956bbeb1ab59534d1e8554da01d2ed
Ilya Skriblovsky <ilyaskriblovsky@gmail.com>
2012-03-21 18:03:04 +0000

    ‘FRS’ translation added

---
+++ fetching/french2/conv.py
@@ -0,0 +1,21 @@
+import sqlite3, glob
+
+transCode = 'frs'
+langCode = 'f'
+
+db = sqlite3.Connection("{0}.sqlite".format(transCode))
+c = db.cursor()
+
+c.execute("DROP TABLE IF EXISTS html")
+c.execute("CREATE TABLE html (langCode, bookCode, chapterNo, html, PRIMARY KEY (langCode, bookCode, chapterNo))")
+
+for f in glob.glob("*.html"):
+    bookCode, chapterNo = f[:-5].split('_')
+    chapterNo = int(chapterNo)
+    print bookCode, chapterNo
+
+    c.execute("INSERT INTO html (langCode, bookCode, chapterNo, html) VALUES (?, ?, ?, ?)", (
+        langCode, bookCode, chapterNo, open(f).read().decode("utf-8")
+    ))
+
+c.execute("VACUUM")
