ec7844acb0db7860d80a4b94a7810d9615ba1e53
Ilya Skriblovsky <ilyaskriblovsky@gmail.com>
2012-03-25 21:00:05 +0400

    Work on IAP donations

--- src/IAPDonation.cpp
+++ src/IAPDonation.cpp
@@ -2,27 +2,67 @@
 
 #include <iapclient.h>
 
+#include <QThread>
 #include <QDebug>
 #include <QElapsedTimer>
 
 
-IAPDonation::IAPDonation(QObject* parent): QObject(parent)
+/*
+class IAPCreationThread: public QThread
 {
-    QElapsedTimer timer; timer.start();
+    public:
+        IAPCreationThread(IAPDonation* parent):
+            QThread(parent), _iapDonation(parent)
+        {
+        }
+
+        void run()
+        {
+            qDebug() << "run() 1";
+            QElapsedTimer timer; timer.start();
+            qDebug() << "run() 2";
+            IAPClient *iap = new IAPClient;
+            qDebug() << "run() 3";
+            qDebug() << "ELAPSED:" << timer.elapsed();
+            qDebug() << "run() 4";
+
+            _iapDonation->iapCreated(iap);
+            qDebug() << "run() 5";
+        }
+
+    private:
+        IAPDonation* _iapDonation;
+};
+*/
+
+
+IAPDonation::IAPDonation(QObject* parent):
+    QObject(parent), _iap(0)
+{
+}
+
+IAPClient* IAPDonation::iap()
+{
+    if (_iap) return _iap;
 
     _iap = new IAPClient(this);
     connect(_iap, SIGNAL(purchaseCompleted(int, QString, QString)), this, SLOT(onPurchaseCompleted(int, QString, QString)));
+    connect(_iap, SIGNAL(purchaseFlowFinished(int)), this, SLOT(onPurchaseFlowFinished(int)));
+
+    return _iap;
 }
 
 void IAPDonation::purchase(QString productId)
 {
-    _iap->purchaseProduct(productId, IAPClient::NoForcedRestoration);
+    iap()->purchaseProduct(productId, IAPClient::NoForcedRestoration);
 }
 
 void IAPDonation::onPurchaseCompleted(int requestId, QString status, QString ticket)
 {
-    qDebug() << "onPurchaseCompleted";
-    qDebug() << "\t" << "requestId" << requestId;
-    qDebug() << "\t" << "status" << status;
-    qDebug() << "\t" << "ticket" << ticket;
+    purchaseCompleted(status == "OK", status == "RestorableProduct");
+}
+
+void IAPDonation::onPurchaseFlowFinished(int requestId)
+{
+    flowFinished();
 }
--- src/IAPDonation.h
+++ src/IAPDonation.h
@@ -15,11 +15,19 @@ class IAPDonation: public QObject
     public slots:
         void purchase(QString productId);
 
+    signals:
+        void purchaseCompleted(bool success, bool alreadyPurchased);
+        void flowFinished();
+
+
     private:
         IAPClient* _iap;
 
+        IAPClient* iap();
+
     private slots:
         void onPurchaseCompleted(int requestId, QString status, QString ticket);
+        void onPurchaseFlowFinished(int requestId);
 };
 
 #endif
