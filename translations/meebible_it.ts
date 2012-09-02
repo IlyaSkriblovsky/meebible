3c99c0e1e7ccb4769d1ba979239d703781e575a9
Ilya Skriblovsky <ilyaskriblovsky@gmail.com>
2012-09-02 19:45:37 +0400

    Italian translation added

---
+++ translations/meebible_it.ts
@@ -0,0 +1,433 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About MeeBible</source>
+        <translation>Informazioni su MeeBible</translation>
+    </message>
+    <message>
+        <source>If you want to support development, please consider buying &lt;a href=&apos;http://store.ovi.com/content/215454&apos;&gt;paid version&lt;/a&gt;!</source>
+        <translation>Per sostenere lo sviluppo, si prega di considerare l'acquisto della &lt;a href=&apos;http://store.ovi.com/content/215454&apos;&gt;versione a pagamento&lt;/a&gt;!</translation>
+    </message>
+    <message>
+        <source>Thank you for supporting development!</source>
+        <translation>Grazie per avere sostenuto lo sviluppo!</translation>
+    </message>
+    <message>
+        <source>© 2011 Ilya Skriblovsky</source>
+        <translation>© 2011 Ilya Skriblovsky</translation>
+    </message>
+    <message>
+        <source>Please contact us with any comments or suggestions or if you want to add new Bible or UI translations.</source>
+        <translation>Si prega di contattarci per commenti, suggerimenti e aggiunta di traduzioni della Bibbia o dell'applicazione.</translation>
+    </message>
+    <message>
+        <source>Send your feedback</source>
+        <translation>Invia il tuo commento</translation>
+    </message>
+    <message>
+        <source>Read God&apos;s Word daily!</source>
+        <translation>Leggi ogni giorno la Parola di Dio!</translation>
+    </message>
+    <message>
+        <source>The open source Bible reader</source>
+        <translation>Il lettore open source della Bibbia</translation>
+    </message>
+</context>
+<context>
+    <name>BibleView</name>
+    <message>
+        <source>Current translation doesn&apos;t contain this book</source>
+        <translation>La traduzione corrente non contiene questo libro</translation>
+    </message>
+    <message>
+        <source>&lt;h3&gt;Cannot load chapter&lt;/h3&gt; Please check your internet connection</source>
+        <translation>&lt;h3&gt;Impossibile caricare il capitolo&lt;/h3&gt; Si prega di verificare la connessione internet</translation>
+    </message>
+</context>
+<context>
+    <name>BibleViewPage</name>
+    <message>
+        <source>Copy</source>
+        <translation>Copia</translation>
+    </message>
+    <message>
+        <source>Share</source>
+        <translation>Condividi</translation>
+    </message>
+    <message>
+        <source>Bookmark</source>
+        <translation>Segnalibro</translation>
+    </message>
+    <message>
+        <source>Bookmarked</source>
+        <translation>Segnalibro aggiunto</translation>
+    </message>
+    <message>
+        <source>Already in bookmarks</source>
+        <translation>Già presente nei segnalibri</translation>
+    </message>
+    <message>
+        <source>Before you start to read, MeeBible has to load translation list from server</source>
+        <translation>Prima di iniziare la lettura, Meebible deve caricare la lista delle traduzioni dal server</translation>
+    </message>
+    <message>
+        <source>Try again</source>
+        <translation>Riprova</translation>
+    </message>
+    <message>
+        <source>Loading chapter</source>
+        <translation>Caricamento capitolo</translation>
+    </message>
+    <message>
+        <source>Loading translation list</source>
+        <translation>Caricamento elenco traduzioni</translation>
+    </message>
+    <message>
+        <source>Loading book list</source>
+        <translation>Caricamento elenco libri</translation>
+    </message>
+    <message>
+        <source>Please notice</source>
+        <translation>Si prega di notare</translation>
+    </message>
+    <message>
+        <source>Full-text search works only for downloaded chapters. If you want to search whole Bible you must download it first using the app menu.</source>
+        <translation>La ricerca nel testo funziona solo per i capitoli scaricati. Per cercare nell'intera Bibbia è necessario prima scaricarla tramite il menu dell'applicazione.</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Cannot load translation list</source>
+        <translation>Impossibile caricare lista traduzioni</translation>
+    </message>
+    <message>
+        <source>Please check internet connection</source>
+        <translation>Si prega di verificare la connessione internet</translation>
+    </message>
+    <message>
+        <source>Done</source>
+        <translation>Fatto</translation>
+    </message>
+    <message>
+        <source>Are you ready to download whole Bible?</source>
+        <translation>Pronto a scaricare l'intera Bibbia?</translation>
+    </message>
+    <message>
+        <source>This may take 10-15 minutes and you&apos;d better do it over Wi-Fi connection</source>
+        <translation>Questo richiederà 10-15 minuti ed è preferibile usare connessione Wi-Fi</translation>
+    </message>
+    <message>
+        <source>Download</source>
+        <translation>Scaricamento</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Annulla</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Cerca</translation>
+    </message>
+    <message>
+        <source>Copied</source>
+        <translation>Copiato</translation>
+    </message>
+    <message>
+        <source>Font size</source>
+        <translation>Dimensione carattere</translation>
+    </message>
+    <message>
+        <source>Line spacing</source>
+        <translation>Interlinea</translation>
+    </message>
+    <message>
+        <source>Download Bible</source>
+        <translation>Scarica Bibbia</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>About MeeBible</source>
+        <translation>Informazioni su MeeBible</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Esci</translation>
+    </message>
+</context>
+<context>
+    <name>BookmarksSheet</name>
+    <message>
+        <source>Bookmarks</source>
+        <translation>Segnalibri</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Cancella</translation>
+    </message>
+    <message>
+        <source>No bookmarks yet</source>
+        <translation>Nessun segnalibro</translation>
+    </message>
+</context>
+<context>
+    <name>FeedbackPage</name>
+    <message>
+        <source>Feedback</source>
+        <translation>Commenti</translation>
+    </message>
+    <message>
+        <source>Your name</source>
+        <translation>Nome</translation>
+    </message>
+    <message>
+        <source>Your E-Mail</source>
+        <translation>E-Mail</translation>
+    </message>
+    <message>
+        <source>Comments
+Suggestions
+Improvements
+Critics
+New translations
+...</source>
+        <translation>Commenti
+Suggerimenti
+Miglioramenti
+Critiche
+Nuove traduzioni
+...</translation>
+    </message>
+    <message>
+        <source>Send</source>
+        <translation>Invia</translation>
+    </message>
+    <message>
+        <source>Sending feedback</source>
+        <translation>Invio commento...</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Annulla</translation>
+    </message>
+    <message>
+        <source>Feedback sent</source>
+        <translation>Commento inviato</translation>
+    </message>
+    <message>
+        <source>Thank you! Your feedback is highly appreciated. We will answer you as soon as possible.</source>
+        <translation>Grazie! Il tuo contributo è molto gradito. Risponderemo appena possibile.</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Sending failed</source>
+        <translation>Invio fallito</translation>
+    </message>
+    <message>
+        <source>Your feedback was not sent. Please check internet connection and try again.</source>
+        <translation>Commento non inviato. Si prega di verificare la connessione Internet e riprovare.</translation>
+    </message>
+    <message>
+        <source>No E-Mail?</source>
+        <translation>Nessuna E-Mail?</translation>
+    </message>
+    <message>
+        <source>You have not specified E-Mail address. We will not be able to answer you!</source>
+        <translation>Indirizzo E-Mail non specificato. Non saremo in grado di rispondere!</translation>
+    </message>
+    <message>
+        <source>Send anyway</source>
+        <translation>Invia comunque</translation>
+    </message>
+    <message>
+        <source>Return to form</source>
+        <translation>Ritorna al modulo</translation>
+    </message>
+</context>
+<context>
+    <name>FetcherDialog</name>
+    <message>
+        <source>Downloading Bible</source>
+        <translation>Scaricamento Bibbia</translation>
+    </message>
+    <message>
+        <source>Connecting...</source>
+        <translation>Connessione in corso...</translation>
+    </message>
+    <message>
+        <source>Nothing to do</source>
+        <translation>Niente da fare</translation>
+    </message>
+    <message>
+        <source>You already have downloaded whole Bible in selected language and translation</source>
+        <translation>L'intera Bibbia nel linguaggio e traduzione selezionati è già stata scaricata</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Annulla</translation>
+    </message>
+</context>
+<context>
+    <name>HelpButton</name>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+<context>
+    <name>LanguageDialog</name>
+    <message>
+        <source>Select Language</source>
+        <translation>Scegli Lingua</translation>
+    </message>
+</context>
+<context>
+    <name>PlaceDialog</name>
+    <message>
+        <source>Select Book</source>
+        <translation>Scegli Libro</translation>
+    </message>
+    <message>
+        <source>Chapter:</source>
+        <translation>Capitolo:</translation>
+    </message>
+    <message>
+        <source>Verse:</source>
+        <translation>Versetto:</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+<context>
+    <name>SearchDialog</name>
+    <message>
+        <source>Close</source>
+        <translation>Chiudi</translation>
+    </message>
+    <message>
+        <source>Search...</source>
+        <translation>Cerca...</translation>
+    </message>
+    <message>
+        <source>Nothing found</source>
+        <translation>Nessun risultato</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>Lingua</translation>
+    </message>
+    <message>
+        <source>&lt;not loaded yet&gt;</source>
+        <translation>&lt;non ancora caricato&gt;</translation>
+    </message>
+    <message>
+        <source>Translation</source>
+        <translation>Traduzione</translation>
+    </message>
+    <message>
+        <source>Font</source>
+        <translation>Carattere</translation>
+    </message>
+    <message>
+        <source>Scroll header with the text</source>
+        <translation>Scorri l'intestazione con il testo</translation>
+    </message>
+    <message>
+        <source>Fullscreen</source>
+        <translation>Schermo intero</translation>
+    </message>
+    <message>
+        <source>Inverted theme</source>
+        <translation>Tema colori invertiti</translation>
+    </message>
+    <message>
+        <source>Copy verses with numbers</source>
+        <translation>Copia versetti con numero</translation>
+    </message>
+    <message>
+        <source>Update translation list</source>
+        <translation>Aggiorna elenco traduzioni</translation>
+    </message>
+    <message>
+        <source>Reload languages</source>
+        <translation>Ricarica lingue</translation>
+    </message>
+    <message>
+        <source>When new translation is published on MeeBible&apos;s server, use this button to update translation list</source>
+        <translation>Quando nuove traduzioni sono pubblicate sul server di MeeBibile, usa questo pulsante per aggiornare l'elenco traduzioni</translation>
+    </message>
+    <message>
+        <source>Clear cache</source>
+        <translation>Cancella cache</translation>
+    </message>
+    <message>
+        <source>Cache clearing</source>
+        <translation>Cancellazione cache...</translation>
+    </message>
+    <message>
+        <source>Select Font</source>
+        <translation>Scelta carattere</translation>
+    </message>
+    <message>
+        <source>Not all fonts can display all languages</source>
+        <translation>Non tutti i caratteri possono visualizzare tutte le lingue</translation>
+    </message>
+    <message>
+        <source>Do you want to clear the cache?</source>
+        <translation>Vuoi cancellare la cache?</translation>
+    </message>
+    <message>
+        <source>This will delete all downloaded Bible chapters from your phone</source>
+        <translation>Questo cancellerà tutti i capitoli della Bibbia scaricati dal telefono</translation>
+    </message>
+    <message>
+        <source>Clear</source>
+        <translation>Cancella</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Annulla</translation>
+    </message>
+    <message>
+        <source>Available at</source>
+        <translation>Disponibile su</translation>
+    </message>
+</context>
+<context>
+    <name>TranslationDialog</name>
+    <message>
+        <source>Select Translation</source>
+        <translation>Scegli Traduzione</translation>
+    </message>
+</context>
+<context>
+    <name>UniSheet</name>
+    <message>
+        <source>Close</source>
+        <translation>Chiudi</translation>
+    </message>
+</context>
+</TS>
